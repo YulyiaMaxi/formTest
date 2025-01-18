9477128d1769528140bcee62da1b35373e200332
unknown <ylvimaksimova@gmailcom>
2025-01-19 02:18:47 +0300

    My commit

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -3,7 +3,7 @@ describe('Тест формы', () => {
     it('Тест с вводом валидных значений', () => {
 
         cy.visit('https://www.testograf.ru/ru/blog/feedback-form-template'); 
-        cy.get('.input.textfield___c13e5c354deb256560e7').eq(0)
+      /*  cy.get('.input.textfield___c13e5c354deb256560e7').eq(0)
           .scrollIntoView()
           .should('be.visible'); 
 
@@ -17,16 +17,16 @@ describe('Тест формы', () => {
         cy.get('.title_bb675eac964120e07a92').contains('Заказ');
 
         cy.get('.input.textfield___c13e5c354deb256560e7').eq(2)
-          .type('Где мой заказ?');
+          .type('Где мой заказ?');*/
 
-        cy.get('.button.button_fd25a2fb454e3f726adc.blue_f42ee3b7578def392092.fluid___b8ec6d1bc6ffd588c6ef')
+        cy.get('div.action.button')
           .click()
           .then(() => {
               cy.contains('Благодарим за обращение!') 
                 .should('be.visible')
                 .then(() => {
                     console.log('Форма успешно отправлена');
-                });
+                })
           });
     });
 });
